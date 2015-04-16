2cc2f12fdc8e14a347efe13b14f818e3e7535ad5
Jeremy Ellis <jellis@hpssapps.com>
2015-04-16 14:21:49 -0700

    Update spark-as-client.ino

--- spark-as-client/spark-as-client.ino
+++ spark-as-client/spark-as-client.ino
@@ -2,16 +2,23 @@ TCPClient client;
 byte server[] = { 74, 125, 224, 72 }; // Google
 void setup()
 {
-  // Make sure your Serial Terminal app is closed before powering your Core
-  Serial.begin(9600);
-  // Now open your Serial Terminal, and hit any key to continue!
-  while(!Serial.available()) SPARK_WLAN_Loop();
+  
+  pinMode(D7, OUTPUT);     // output means information out of the spark device
 
-  Serial.println("connecting...");
+  // Make sure your Serial Terminal app is closed before powering your Core
+ // Serial.begin(9600);
+  // Now open your Serial Terminal, and hit any key to continue!
+  //while(!Serial.available()) 
+  
+  //SPARK_WLAN_Loop();
+
+  //Serial.println("connecting...");
 
   if (client.connect(server, 80))
   {
-    Serial.println("connected");
+
+  
+   // Serial.println("connected");
     client.println("GET /search?q=unicorn HTTP/1.0");
     client.println("Host: www.google.com");
     client.println("Content-Length: 0");
@@ -19,7 +26,7 @@ void setup()
   }
   else
   {
-    Serial.println("connection failed");
+    //Serial.println("connection failed");
   }
 }
 
@@ -27,15 +34,16 @@ void loop()
 {
   if (client.available())
   {
+    digitalWrite(D7, HIGH);  // send 1 high 0 low
     char c = client.read();
     Serial.print(c);
   }
 
   if (!client.connected())
   {
-    Serial.println();
-    Serial.println("disconnecting.");
+    //Serial.println();
+    //Serial.println("disconnecting.");
     client.stop();
-    for(;;);
+    for(;;);     // what the heck does this do???
   }
 }
